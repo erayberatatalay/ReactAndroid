3dc502743632224603859fcde6f681a577f3bb6b
erayberatatalay <erayberat37@gmail.com>
2023-01-19 15:10:03 +0300

    Initial commit

---
+++ android/app/src/main/java/io/ionic/react_project/MainActivity.java
@@ -0,0 +1,5 @@
+package io.ionic.react_project;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ src/App.js
@@ -0,0 +1,16 @@
+//import logo from './logo.svg';
+import logo from './iyzi.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} alt="logo" />
+        <p>iyziShip</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
